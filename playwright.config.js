86087b6c9e8d6b8491128ffeb97fea1f669eb953
D <bhushanmali@MacBook-Air.local>
2025-07-08 20:49:38 +0530

    config file changes

--- playwright.config.js
+++ playwright.config.js
@@ -26,9 +26,9 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: [
-  ['list'],                     // Terminal output साठी
-  ['html'],                     // Optional - HTML report
-  ['allure-playwright']         // ✅ मुख्य: Allure report साठी
+  ['list'],                     
+  ['html'],                     
+  ['allure-playwright']         
 ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
